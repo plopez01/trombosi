6a45af3309c0b91feb42b029f8fe4fac3b5cd7c3
altrisi <altrisi.trillosierra@gmail.com>
2022-12-17 16:44:28 +0100

    Screen change delay

--- Assets/Scripts/MainMenu/PanelManager.cs
+++ Assets/Scripts/MainMenu/PanelManager.cs
@@ -6,19 +6,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-
 public class PanelManager : MonoBehaviour
 {
-    [SerializeField] private FadeablePanel currentPanel; // fading in panel
+    [SerializeField] private FadeablePanel currentPanel; // also fading in panel, nullable cuando cambiando a scene
+    [SerializeField, Range(0, 3)] private float fadeTime;
+    [SerializeField, Range(0, 3)] private float screenExtraWait;
     private FadeablePanel fadingOutPanel;
     private AsyncOperation screenBeingOpened;
     private float alpha = 0;
-    private float fadeStartTime;
+    private float lastFadeTime;
 
     public void changeToPanel(FadeablePanel panel)
     {
         panel.gameObject.SetActive(true);
-        fadeStartTime = Time.unscaledTime;
+        lastFadeTime = Time.unscaledTime;
         alpha = 0;
         fadingOutPanel = this.currentPanel;
         print("Fading " + currentPanel + " into " + panel);
@@ -26,7 +27,7 @@ public class PanelManager : MonoBehaviour
     }
     public void fadeToScreen(String screen)
     {
-        fadeStartTime = Time.unscaledTime;
+        lastFadeTime = Time.unscaledTime;
         alpha = 0;
         fadingOutPanel = this.currentPanel;
         print("Fading " + currentPanel + " into screen: " + screen);
@@ -37,8 +38,7 @@ public class PanelManager : MonoBehaviour
 
     private void Start()
     {
-        fadeStartTime = Time.unscaledTime + 5;
-        //print(currentPanel);
+        lastFadeTime = Time.unscaledTime + 5;
         // Workaround para no va si no está activo al inicio
         foreach (FadeablePanel p in GetComponentsInChildren<FadeablePanel>())
         {
@@ -52,11 +52,12 @@ public class PanelManager : MonoBehaviour
     {
         if (alpha < 1)
         {
-            alpha = Mathf.Min(1, alpha + (Time.unscaledTime - fadeStartTime)/2);
+            alpha = Mathf.Min(1, alpha + (Time.unscaledTime - lastFadeTime)/fadeTime);
+            lastFadeTime = Time.unscaledTime;
             if (currentPanel != null) currentPanel.setAlpha(alpha);
             if (fadingOutPanel != null)
                 fadingOutPanel.setAlpha(1- alpha);
-        } else if (screenBeingOpened != null)
+        } else if (screenBeingOpened != null && (Time.unscaledTime - lastFadeTime) > screenExtraWait)
         {
             AsyncOperation op = screenBeingOpened;
             screenBeingOpened = null;
