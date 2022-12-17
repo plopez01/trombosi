3cfd038b0a7e2c8a8a3f1d16ab83c6ed3ffa56ba
altrisi <altrisi.trillosierra@gmail.com>
2022-12-17 16:06:46 +0100

    Push whatever I just did about scenes

--- Assets/Scripts/MainMenu/PanelManager.cs
+++ Assets/Scripts/MainMenu/PanelManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -10,17 +11,30 @@ public class PanelManager : MonoBehaviour
 {
     [SerializeField] private FadeablePanel currentPanel; // fading in panel
     private FadeablePanel fadingOutPanel;
+    private AsyncOperation screenBeingOpened;
     private float alpha = 0;
     private float fadeStartTime;
 
     public void changeToPanel(FadeablePanel panel)
     {
+        panel.gameObject.SetActive(true);
         fadeStartTime = Time.unscaledTime;
         alpha = 0;
         fadingOutPanel = this.currentPanel;
         print("Fading " + currentPanel + " into " + panel);
         this.currentPanel = panel;
     }
+    public void fadeToScreen(String screen)
+    {
+        fadeStartTime = Time.unscaledTime;
+        alpha = 0;
+        fadingOutPanel = this.currentPanel;
+        print("Fading " + currentPanel + " into screen: " + screen);
+        screenBeingOpened = SceneManager.LoadSceneAsync(screen);
+        screenBeingOpened.allowSceneActivation = false;
+        this.currentPanel = null;
+    }
+
     private void Start()
     {
         fadeStartTime = Time.unscaledTime + 5;
@@ -39,9 +53,17 @@ public class PanelManager : MonoBehaviour
         if (alpha < 1)
         {
             alpha = Mathf.Min(1, alpha + (Time.unscaledTime - fadeStartTime)/2);
-            currentPanel.setAlpha(alpha);
+            if (currentPanel != null) currentPanel.setAlpha(alpha);
             if (fadingOutPanel != null)
                 fadingOutPanel.setAlpha(1- alpha);
+        } else if (screenBeingOpened != null)
+        {
+            AsyncOperation op = screenBeingOpened;
+            screenBeingOpened = null;
+            op.allowSceneActivation = true;
+        } else
+        {
+            if (fadingOutPanel != null) fadingOutPanel.gameObject.SetActive(false);
         }
     }
 }
