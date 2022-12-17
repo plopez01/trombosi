e2215ec9533988a8fe0ea9afbc269faf6b2b9ac8
Plopez_ <plopez.01.info@gmail.com>
2022-12-17 12:10:25 +0100

    Valve

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField, Range(0.1f, 4)] private float pulseInterval;
+
+    [SerializeField] private Animator valveAnimator;
+
+    private float timer = 0;
+    private bool valveOpen = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        valveAnimator.speed = 1 / pulseInterval;
+        if (Time.unscaledTime - timer >= pulseInterval)
+        {
+            timer = Time.unscaledTime;
+            valveOpen = !valveOpen;
+
+
+            valveAnimator.SetBool("Open", valveOpen);
+            Debug.Log("Valve open: " + valveOpen);
+        }
     }
 }
